f97e89f641956cbd3cb6dae51982d874d59d5673
Tram <tm.nguyen1900@gmail.com>
2020-09-02 21:08:35 -0400

    created JobCard component and add functionality

---
+++ src/JobCard.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+/** JobCard
+ * 
+ * Props:
+ * - job: object about a job 
+ *          like {title, salary, equity}
+ * 
+ * State:
+ *  - appliedStatus, false by default
+ * 
+ * JobCardList --> JobCard
+ */
+function JobCard({ job }) {
+  const [applyStatus, setApplyStatus] = useState(false);
+
+
+  function handleClick(evt){
+    setApplyStatus(true);
+  }
+
+  return (
+    <div className="JobCard">
+      <p> {job.title} </p>
+      <p>{job.companyName}</p>
+      <p>Salary: {job.salary}</p>
+      <p>equity: {job.equity}</p>
+
+      {!applyStatus && <button onClick={handleClick} >Apply</button>}
+      {applyStatus && <button disabled>Applied</button>}
+      
+    </div>
+  )
+}
+
+export default JobCard;
