f6f656cc065d7292bfdddcce94c60a047c7e5eb8
Tram <tm.nguyen1900@gmail.com>
2020-09-02 20:55:24 -0400

    added notes token state and useEffect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Routes from './Routes';
 
 function App() {
+    //add token as state
+    //state for currentUser
+
+    //useEffect get backend info and store token
+
   return (
     <div className="App">
       <Routes />
