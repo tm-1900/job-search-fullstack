603cbbc2dc4d38705ac1cd2fe1c83acb20623338
Tram <tm.nguyen1900@gmail.com>
2020-09-02 21:03:49 -0400

    added joblist component functionality and docstrings to function.

--- src/JobList.js
+++ src/JobList.js
@@ -1,20 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SearchForm from './SearchForm';
+import JobCard from './JobCard';
+import JoblyApi from './api';
+import JobCardList from "./JobCardList";
 
-
+//todo. change joblist to jobslist
 /**
+ * Renders JobList.
+ *    - by default, all jobs will be shown
+ *    - If user types inputs into SearchBox, JobList will be filtered
+ *          based on inputs
+ *   
  * Props:
- * - searchJobs: search function received from parent,
- *   passed to SearchForm
- * - jobs: array of job objects like 
- *   [{id, title, salary, equity, companyHandle, companyName}, ...]
+ * - setJobs: received function from parent; will set new jobs list
+ *      when user search for a particular job.
+ * - jobs: array of job objects 
+ *      like [{id, title, salary, equity, companyHandle, companyName}, ...]
+ * 
+ * State:
+ *  - searchJobInput
+ *  - error
  * 
  * Routes --> JobList --> {SearchForm, JobCardList}
  */
-function JobList({ searchJobs, jobs }) {
 
-  // renders JobCard based on jobs
+function JobList({ jobs, setJobs }) {
+  console.log("this this jobslist", jobs)
+
+  const [searchJobInput, setSearchJobInput] = useState({});
+  const [error, setError] = useState(null);
+
+
+  /** Gets data from SearchForm on JobList page, 
+    */
+  function searchJobs(formData) {
+    // console.log("searchJobs ran")
+    setSearchJobInput(formData);
+
+  }
+
+  /**
+   * Makes an api request based on search input, 
+   *  setJobs with api response.
+   */
+  useEffect(function fetchSearchedJobs() {
+    async function fetchJobs() {
+      let result;
+      try {
+        result = await JoblyApi.getJobsWithFilter(searchJobInput)
+        //console.log("this is search input", searchJobInput)
+        //console.log("this is result in fetchSearchedJobs", result)
+
+        if (result.length === 0) {
+          throw new Error("Sorry, no results were found!")
+        }
+
+        setJobs(result)
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+    fetchJobs();
+
+  }, [setJobs, searchJobInput])
+
+
+  /**Handles loading, errors, and JobCardList and renders accordingly. */
+  function showLoadingOrJobs() {
+    if (jobs === null) {
+      return (<p>Loading...</p>)
+    } else if (error) {
+      return (<p> {error} </p>)
+    } else {
+      return (
+        <div>
+          <JobCardList jobs={jobs}/>
+        </div>
+      )
+    }
+  }
+
+  // renders JobCard based on jobs inputs
+  //submitSearch passed a a prop from SearchForm, feeding it searchJobs function
   return (
-    <p>JobList</p>
+    <div>
+      <SearchForm submitSearch={searchJobs} />
+      {showLoadingOrJobs()}
+    </div>
+
   )
 }
 
