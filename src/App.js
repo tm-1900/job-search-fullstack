69c3d46d4526ddce6098b4535e306166abfc5217
Tram <tm.nguyen1900@gmail.com>
2020-10-28 18:07:15 -0400

    moved files around

--- src/App.js
+++ src/App.js
@@ -1,94 +1,105 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Routes from './Routes';
+import Routes from './routes-nav/Routes';
 import "bootstrap/dist/css/bootstrap.css";
-import UserContext from './UserContext';
-import JoblyApi from "./api";
+import UserContext from './auth/UserContext';
+import JoblyApi from "./api/api.js"
 import { BrowserRouter } from "react-router-dom"
-import Navbar from './Navbar'
-
+import Navbar from './routes-nav/Navbar'
 import jwt from "jsonwebtoken"
 
+// Key name to store token in localStorage, to remember the user for re-login
+export const TOKEN_STORAGE_ID = "userToken";
 
 /**
  * State:
- * - currentUser
- * - currentUserToken
+ * - currentUser - from API, used to tell if a user is logged in and is passed
+ *  around through context throughout the app.
  * 
+ * - currentUserToken - this is the token for logged in user, the user's 
+ *  authentication JWT. It's required to be set for most API calls.
+ * 
+ * App --> Routes
  */
 function App() {
-  //add token as state
   const [currentUser, setCurrentUser] = useState(null);
-  const [currentUserToken, setCurrentUserToken] = useState(null);
+
+  //get initial currentUserToken from localStorage
+  const initialToken = localStorage.getItem(TOKEN_STORAGE_ID)
+  const [currentUserToken, setCurrentUserToken] = useState(initialToken);
+  const [applicationIds, setApplicationIds] = useState(new Set([]));
+
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  // console.log('this is current user App', currentUser)
-  //console.log('this is token', currentUserToken)
+  /** Load user info from API. This effect will only run if a user is logged
+   * in and have a token. The token will be saved to localStorage. 
+   * This will re-run when the user logs out, this means the value of 
+   * the token is a dependency for the useEffect.
+   */
+  useEffect(function loadUserInfoWithToken() {
+    console.debug("App useEffect loadUserInfoWithToken",
+      "currentUserToken=", currentUserToken);
 
-
-  useEffect(function getCurrentUserFromToken() {
+    // set currentUser token to localStorage
+    if (currentUserToken) localStorage.setItem(TOKEN_STORAGE_ID, initialToken)
+    const tokenFromLS = localStorage.getItem(TOKEN_STORAGE_ID);
+   
     async function getUserFromToken() {
-      try {
-        const token = localStorage.getItem('user');
-        JoblyApi.token = token
+      if (tokenFromLS){
+        try {
+          // decode the tokenFromLS and get username
+          const { username } = jwt.decode(tokenFromLS);
 
-        // decode the token and get username
-        const user = jwt.decode(token);
-        console.log("this is user in getUserFromToken", user)
+          // save tokenFromLS to API class
+          JoblyApi.token = tokenFromLS;
 
-        if (user) {
           // make an api request for the user info using the username
-          const userDetail = await JoblyApi.getUser(user.username)
-          console.log("this is userDetail", userDetail)
-    
-          // setCurrentUser to response of api
-          setCurrentUser(userDetail);
-        }
-  
-      } catch(err) {
-        setError(err);
-        console.log("this is err in getUserFromToken", err)
-      } finally {
-        setIsLoading(false);
+          let currentUser1 = await JoblyApi.getUser(username);
+          // setCurrentUser based on response of api
+          setCurrentUser(currentUser1);
+        } catch (err) {
+          setError(err);
+        } 
+        setIsLoading(true);
       }
     }
+    setIsLoading(true);
     getUserFromToken();
   }, [currentUserToken])
 
+
   /** Reset user state be empty object. */
   function logoutUser() {
-    setCurrentUserToken(null);
+    localStorage.removeItem(TOKEN_STORAGE_ID);
     setCurrentUser(null)
-    localStorage.removeItem('user');
+    setCurrentUserToken(null);
   }
 
   async function login(formData) {
-    const token = await JoblyApi.getToken(formData);
+    const token = await JoblyApi.login(formData);
     // const userDetail = await JoblyApi.getUser(formData.username);
-    
+
     // TODO why does this order matter?
     localStorage.setItem('user', token);
     setCurrentUserToken(token);
     // console.log('this is userDetail', userDetail)
   }
 
-  async function signUp(formData){
-    const newUserToken = await JoblyApi.registerUser(formData);
+  async function signup(formData) {
+    const newUserToken = await JoblyApi.signup(formData);
 
     localStorage.setItem('user', newUserToken);
     setCurrentUserToken(newUserToken);
   }
 
   // to remember a logged in user, we need a useEffect to get the user via the API using the token
-  // 
-
   function showLoadingOrNavbar() {
     if (isLoading) return (<p>Loading...</p>);
     if (error) return (<p> {error} </p>);
   }
 
-  
+
   return (
     <div className="App">
       {/* <nav class="Navigation navbar navbar-expand-md"> */}
@@ -96,8 +107,8 @@ function App() {
         {showLoadingOrNavbar()}
         <UserContext.Provider value={currentUser}>
           <Navbar logoutUser={logoutUser} />
-          <Routes signUp={signUp}
-                  login={login}
+          <Routes signUp={signup}
+            login={login}
           />
         </UserContext.Provider>
       </BrowserRouter>
