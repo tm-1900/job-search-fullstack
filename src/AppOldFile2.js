2567cc1dfd99de75ce0f716b0db397dfcc199629
Tram <tm.nguyen1900@gmail.com>
2020-10-29 20:32:22 -0400

    copy of old files

---
+++ src/AppOldFile2.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Routes from './routes-nav/Routes';
+import "bootstrap/dist/css/bootstrap.css";
+import UserContext from './auth/UserContext';
+import JoblyApi from "./api/api.js"
+import { BrowserRouter } from "react-router-dom"
+import Navbar from './routes-nav/Navbar'
+import jwt from "jsonwebtoken"
+
+// Key name to store token in localStorage, to remember the user for re-login
+export const TOKEN_STORAGE_ID = "userToken";
+
+/**
+ * State:
+ * - currentUser - from API, used to tell if a user is logged in and is passed
+ *  around through context throughout the app.
+ * 
+ * - currentUserToken - this is the token for logged in user, the user's 
+ *  authentication JWT. It's required to be set for most API calls.
+ * 
+ * App --> Routes
+ */
+function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  //{userToken: lkajdjafla;fadf} localStorage.TOKEN_STORAGE_ID = lkajdjafla;fadf
+
+  //get initial currentUserToken from localStorage
+  const initialToken = localStorage.getItem(TOKEN_STORAGE_ID) || null;
+
+  const [currentUserToken, setCurrentUserToken] = useState(initialToken);
+  const [applicationIds, setApplicationIds] = useState(new Set([]));
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  /** Load user info from API. This effect will only run if a user is logged
+   * in and have a token. The token will be saved to localStorage. 
+   * This will re-run when the user logs out, this means the value of 
+   * the token is a dependency for the useEffect.
+   */
+  useEffect(function loadUserInfoWithToken() {
+    console.debug("App useEffect loadUserInfoWithToken",
+      "currentUserToken=", currentUserToken);
+
+    // set currentUser token to localStorage
+    if (currentUserToken) localStorage.setItem(TOKEN_STORAGE_ID, currentUserToken)
+    const tokenFromLS = localStorage.getItem(TOKEN_STORAGE_ID);
+   
+    async function getUserFromToken() {
+      console.debug("App useEffect getUserFromToken",
+        "currentUserToken=", currentUserToken);
+      if (tokenFromLS){
+        try {
+          // decode the tokenFromLS and get username
+          const  {username}  = jwt.decode(tokenFromLS);
+
+          // save tokenFromLS to API class
+          JoblyApi.token = tokenFromLS;
+
+          // make an api request for the user info using the username
+          let currentUser1 = await JoblyApi.getUser(username);
+          console.debug("App useEffect getUserFromToken",
+            "currentUser1=", currentUser1);
+
+          // setCurrentUser based on response of api
+          setCurrentUser(currentUser1);
+        } catch (err) {
+          setError(err);
+        } 
+        setIsLoading(true);
+      }
+    }
+    setIsLoading(true);
+    getUserFromToken();
+  }, [currentUserToken])
+
+
+  /** Reset user state be empty object. */
+  function logoutUser() {
+    localStorage.removeItem(TOKEN_STORAGE_ID);
+    setCurrentUser(null)
+    setCurrentUserToken(null);
+  }
+
+  async function login(formData) {
+    const token = await JoblyApi.login(formData);
+    // const userDetail = await JoblyApi.getUser(formData.username);
+
+    // TODO why does this order matter?
+    localStorage.setItem(TOKEN_STORAGE_ID, token); //user: kldjldjaldkfja
+    setCurrentUserToken(token);
+    // console.log('this is userDetail', userDetail)
+  }
+
+  async function signup(formData) {
+    const newUserToken = await JoblyApi.signup(formData);
+
+    localStorage.setItem('user', newUserToken);
+    setCurrentUserToken(newUserToken);
+  }
+
+  // to remember a logged in user, we need a useEffect to get the user via the API using the token
+  function showLoadingOrNavbar() {
+    if (isLoading) return (<p>Loading...</p>);
+    if (error) return (<p> {error} </p>);
+  }
+
+  ///testing this line
+
+  return (
+    <div className="App">
+      {/* <nav class="Navigation navbar navbar-expand-md"> */}
+      <BrowserRouter>
+        {showLoadingOrNavbar()}
+        <UserContext.Provider value={{currentUser}}>
+          <Navbar logoutUser={logoutUser} />
+          <Routes signup={signup}
+            login={login}/>
+        </UserContext.Provider>
+      </BrowserRouter>
+      {/* </nav> */}
+    </div>
+  );
+}
+
+export default App;
