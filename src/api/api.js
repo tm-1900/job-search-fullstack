2832898a6587c507147e8f565ecae3bf16ee7e64
Tram <tm.nguyen1900@gmail.com>
2020-10-30 20:28:39 -0400

    change localhost

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://nopenopenope";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
